508991c23d41c91b29b8cdce501588839e2abb55
Massimiliano <massimiliano.giovagnoli@innoteam.it>
2020-04-05 16:46:53 +0200

    fix(cluster): add egress rule to cluster SG

--- iac/cluster.tf
+++ iac/cluster.tf
@@ -64,6 +64,13 @@ resource "aws_security_group" "cluster_master_nodes" {
     cidr_blocks = [var.cluster_ssh_allowed_ip_class]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = local.tags
 }
 
@@ -78,6 +85,13 @@ resource "aws_security_group" "cluster_worker_nodes" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.cluster_ssh_allowed_ip_class]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = local.tags
